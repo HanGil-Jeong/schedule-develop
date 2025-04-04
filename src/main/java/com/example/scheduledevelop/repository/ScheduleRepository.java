c92a182d019d36b1576c9c3fc7b2b7c515b8d8d2
HanGil <jhg9240@gmail.com>
2025-04-04 09:10:17 +0900

    feat: 특정 유저 조회 기능 추가

--- src/main/java/com/example/scheduledevelop/repository/ScheduleRepository.java
+++ src/main/java/com/example/scheduledevelop/repository/ScheduleRepository.java
@@ -7,7 +7,13 @@ import org.springframework.web.server.ResponseStatusException;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    default Schedule findByIdOrElseThrow(Long id){
-        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다"));
+    default Schedule findByIdOrElseThrow(Long id) {
+
+        return findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다"
+                        )
+                );
     }
 }
